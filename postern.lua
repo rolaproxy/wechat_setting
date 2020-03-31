3187086c3741ff7455a18e3f6f2a51f869f3d6a6
rolaproxy <62020619+rolaproxy@users.noreply.github.com>
2020-03-31 21:47:15 +0800

    Update postern.lua

--- postern.lua
+++ postern.lua
@@ -10,17 +10,7 @@ exclude-simple-hostnames = true
 
 [Rule]
 DOMAIN-SUFFIX,rola-ip.site,DIRECT
-DOMAIN-SUFFIX,google.com,REJECT
-DOMAIN-SUFFIX,facebook.com,REJECT
-DOMAIN-SUFFIX,twitter.com,REJECT
-DOMAIN-SUFFIX,instagram.com,REJECT
-DOMAIN-SUFFIX,youtube.com,REJECT
-DOMAIN-SUFFIX,google.cn,REJECT
-DOMAIN-SUFFIX,res.wx.qq.com,DIRECT
-DOMAIN-SUFFIX,bugly.qq.com,DIRECT
-DOMAIN-SUFFIX,wup.imtt.qq.com,DIRECT
-DOMAIN-SUFFIX,beacon.qq.com,DIRECT
-DOMAIN-SUFFIX,dldir1.qq.com,DIRECT
 DOMAIN-SUFFIX,tbs.imtt.qq.com,DIRECT
-DOMAIN-SUFFIX,come-ip.site,DIRECT
+DOMAIN-SUFFIX,dldir1.qq.com,DIRECT
+DOMAIN-SUFFIX,safebrowsing.googleapis.com,DIRECT
 FINAL,1
